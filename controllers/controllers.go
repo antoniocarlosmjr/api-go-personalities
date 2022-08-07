94f3d7fa36e391319a5e5f69e28fa7bc61409ce0
Antonio Martins <antonio.pereira@mercadolivre.com>
2022-08-07 16:56:31 -0300

    feat: add todos

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -13,12 +13,14 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Home Page")
 }
 
+// GetPersonalities Todo: remove acces to database this and put in a repository, for example
 func GetPersonalities(w http.ResponseWriter, r *http.Request) {
 	var personalities []models.Personality
 	database.DB.Find(&personalities)
 	json.NewEncoder(w).Encode(personalities)
 }
 
+// GetPersonalityById todo: remove acces to database this and put in a repository, for example
 func GetPersonalityById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
